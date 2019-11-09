111ae0428755d60ca5dd164dc7319b2134d45cbe
Nyakinyua <wanyakinyua968@gmail.com>
2019-11-09 05:11:51 +0300

    run first test new_user and test the parameters using unittest in build function assertEqual()

--- user_test.py
+++ user_test.py
@@ -19,7 +19,10 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_user.first_name,"Joyce")
         self.assertEqual(self.new_user.sur_name,"Nyakinyua")
         self.assertEqual(self.new_user.mobile_number,"0721317715")
-        self.assertEqual(self.new_user.new_password,"Joynya123")
+        self.assertEqual(self.new_user.new_password,"joynya123")
+
+if __name__ == '__main__':
+    unittest.main()
 
 
         
